266430d3013e440471316e65e5c1f544db83b032
zabih5165-jpg <zabih5165@gmail.com>
2025-10-17 05:43:29 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInnumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInnumber );
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1; false => 0
+
+let isLoggedIn = "Zabi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => ture; 0 => false
+// "" => false
+// "Zabi" => ture
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
