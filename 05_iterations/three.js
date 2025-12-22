f5d70c8dcbd1eadf4588dabd7766ec9d83ce87b8
zabih5165-jpg <zabih5165@gmail.com>
2025-12-22 06:06:00 +0000

    06_dom

---
+++ 05_iterations/three.js
@@ -0,0 +1,40 @@
+// for of 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4,5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+
+
+
+
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// console.log(map);
+
+//for ((const [key, value] of map) {
+//    console.log(key, ':-', value);
+//}
+
+
+
+const myObject = {
+    Game1 : 'NFS',
+    Game2 : 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//    console.log(key, ':-', value);
+// }
